940cc907b7423efb6e069a3232717b03e8d4e686
Spyros Thanos <spyrosthanos@Spyross-MacBook-Air.local>
2025-06-29 18:00:44 +0300

    Fixed env file

--- config.js
+++ config.js
@@ -3,11 +3,4 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection(process.env.SQL_URL)
 
-/*const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});*/
-
 module.exports = db;
